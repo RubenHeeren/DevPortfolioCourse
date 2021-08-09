463d3ea4a0518928cc16c3f406d0287c72ddf11a
Cullington <rubenhee1@gmail.com>
2021-08-09 15:16:26 +0200

    Creating the Category model

---
+++ Shared/Models/Category.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Shared.Models
+{
+    class Category
+    {
+        [Key]
+        public int CategoryId { get; set; }
+
+        [Required]
+        [MaxLength(256)]
+        public string ThumbnailImagePath { get; set; }
+
+        [Required]
+        [MaxLength(128)]
+        public string Name { get; set; }
+
+        [Required]
+        [MaxLength(1024)]
+        public string Description { get; set; }
+    }
+}
