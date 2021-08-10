75a411dd43308ecd5b33ac2b3d30006d4bfad6bd
Cullington <rubenhee1@gmail.com>
2021-08-10 14:00:26 +0200

    Consuming the API from the Client (2/2)

--- Client/Services/InMemoryDatabaseCache.cs
+++ Client/Services/InMemoryDatabaseCache.cs
@@ -1,4 +1,5 @@
-﻿using Shared.Models;
+﻿using Client.Static;
+using Shared.Models;
 using System;
 using System.Collections.Generic;
 using System.Net.Http;
@@ -30,9 +31,16 @@ namespace Client.Services
             }
         }
 
+        private bool _gettingCategoriesFromDatabaseAndCaching = false;
         internal async Task GetCategoriesFromDatabaseAndCache()
         {
-            _categories = await _httpClient.GetFromJsonAsync<List<Category>>("endpoint");
+            // Only allow one Get request to run at a time.
+            if (_gettingCategoriesFromDatabaseAndCaching == false)
+            {
+                _gettingCategoriesFromDatabaseAndCaching = true;
+                _categories = await _httpClient.GetFromJsonAsync<List<Category>>(APIEndpoints.s_categories);
+                _gettingCategoriesFromDatabaseAndCaching = false;
+            }            
         }
 
         internal event Action OnCategoriesDataChanged;
---
+++ Client/Static/APIEndpoints.cs
@@ -0,0 +1,13 @@
+﻿namespace Client.Static
+{
+    internal static class APIEndpoints
+    {
+#if DEBUG
+        internal const string ServerBaseUrl = "https://localhost:5003";
+#else
+        internal const string ServerBaseUrl = "https://appname.azurewebsites.net";
+#endif
+
+        internal readonly static string s_categories = $"{ServerBaseUrl}/api/categories";
+    }
+}
