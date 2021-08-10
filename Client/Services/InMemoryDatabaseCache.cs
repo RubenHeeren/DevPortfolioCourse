e871ad1edecc74c117253dd7a0babe18548077eb
Cullington <rubenhee1@gmail.com>
2021-08-10 13:47:57 +0200

    Consuming the API from Client (1/2)

---
+++ Client/Services/InMemoryDatabaseCache.cs
@@ -0,0 +1,41 @@
+﻿using Shared.Models;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+
+namespace Client.Services
+{
+    internal sealed class InMemoryDatabaseCache
+    {
+        private readonly HttpClient _httpClient;
+
+        public InMemoryDatabaseCache(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        private List<Category> _categories = null;
+        internal List<Category> Categories
+        {
+            get
+            {
+                return _categories;
+            }
+            set
+            {
+                _categories = value;
+                NotifyCategoriesDataChanged();
+            }
+        }
+
+        internal async Task GetCategoriesFromDatabaseAndCache()
+        {
+            _categories = await _httpClient.GetFromJsonAsync<List<Category>>("endpoint");
+        }
+
+        internal event Action OnCategoriesDataChanged;
+        private void NotifyCategoriesDataChanged() => OnCategoriesDataChanged?.Invoke();
+    }
+}
