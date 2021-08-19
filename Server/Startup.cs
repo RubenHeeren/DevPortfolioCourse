464f07a67576ad5554be85a9152071e34422096c
Cullington <rubenhee1@gmail.com>
2021-08-19 14:04:56 +0200

    Fixing the object reference cycle error

--- Server/Startup.cs
+++ Server/Startup.cs
@@ -41,7 +41,7 @@ namespace Server
             });
 
             services.AddRouting(options => options.LowercaseUrls = true);
-            services.AddControllers();
+            services.AddControllers().AddNewtonsoftJson(options => options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
