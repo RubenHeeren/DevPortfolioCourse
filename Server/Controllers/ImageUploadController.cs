450ab4849670f51f298a5a5665c0e1e00b5cbb1c
Cullington <70056723+RubenHeeren@users.noreply.github.com>
2021-09-07 15:38:23 +0200

    Adding image uploading (1/2)

---
+++ Server/Controllers/ImageUploadController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Shared.Models;
+using System;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ImageUploadController : ControllerBase
+    {
+        private readonly IWebHostEnvironment _webHostEnvironment;
+
+        public ImageUploadController(IWebHostEnvironment webHostEnvironment)
+        {
+            _webHostEnvironment = webHostEnvironment;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] UploadedImage uploadedImage)
+        {
+            try
+            {
+                if (ModelState.IsValid == false)
+                {
+                    return BadRequest(ModelState);
+                }
+
+                if (uploadedImage.OldImagePath != string.Empty)
+                {
+                    if (uploadedImage.OldImagePath != "uploads/placeholder.jpg")
+                    {
+                        string oldUploadedImageFileName = uploadedImage.OldImagePath.Split('/').Last();
+
+                        System.IO.File.Delete($"{_webHostEnvironment.ContentRootPath}\\wwwroot\\uploads\\{oldUploadedImageFileName}");
+                    }
+                }
+
+                string guid = Guid.NewGuid().ToString();
+                string imageFileName = guid + uploadedImage.NewImageFileExtension;
+
+                string fullImageFileSystemPath = $"{_webHostEnvironment.ContentRootPath}\\wwwrooot\\uploads\\{imageFileName}";
+
+                FileStream fileStream = System.IO.File.Create(fullImageFileSystemPath);
+                byte[] imageContentAsByteArray = Convert.FromBase64String(uploadedImage.NewImageBase64Content);
+                await fileStream.WriteAsync(imageContentAsByteArray, 0, imageContentAsByteArray.Length);
+                fileStream.Close();
+
+                string relativeFilePathWithoutTrailingSlashes = $"uploads/{imageFileName}";
+                return Created("Create", relativeFilePathWithoutTrailingSlashes);
+            }
+            catch (Exception e)
+            {
+                return StatusCode(500, $"Something went wrong on our side. Please contact the administrator. Error message: {e.Message}");
+            }
+        }
+    }
+}
---
+++ Shared/Models/UploadedImage.cs
@@ -0,0 +1,10 @@
+﻿namespace Shared.Models
+{
+    public class UploadedImage
+    {
+        public string NewImageFileExtension { get; set; }
+        // Base64 is basically a string that represents binary
+        public string NewImageBase64Content { get; set; }
+        public string OldImagePath { get; set; }
+    }
+}
