72c0427372f2d7ec6b4f0c1f4d6eaf0a97a373d4
Cullington <70056723+RubenHeeren@users.noreply.github.com>
2021-09-07 16:08:08 +0200

    Adding image uploading (2/2)

--- Client/Static/APIEndpoints.cs
+++ Client/Static/APIEndpoints.cs
@@ -9,5 +9,6 @@
 #endif
 
         internal readonly static string s_categories = $"{ServerBaseUrl}/api/categories";
+        internal readonly static string s_imageUpload = $"{ServerBaseUrl}/api/imageupload";
     }
 }
--- Server/Controllers/ImageUploadController.cs
+++ Server/Controllers/ImageUploadController.cs
@@ -42,7 +42,7 @@ namespace Server.Controllers
                 string guid = Guid.NewGuid().ToString();
                 string imageFileName = guid + uploadedImage.NewImageFileExtension;
 
-                string fullImageFileSystemPath = $"{_webHostEnvironment.ContentRootPath}\\wwwrooot\\uploads\\{imageFileName}";
+                string fullImageFileSystemPath = $"{_webHostEnvironment.ContentRootPath}\\wwwroot\\uploads\\{imageFileName}";
 
                 FileStream fileStream = System.IO.File.Create(fullImageFileSystemPath);
                 byte[] imageContentAsByteArray = Convert.FromBase64String(uploadedImage.NewImageBase64Content);
