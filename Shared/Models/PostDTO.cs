f07ae22d40ea8bedc876f62c19174033c70b5e9a
Cullington <70056723+RubenHeeren@users.noreply.github.com>
2021-09-22 14:29:36 +0200

    Adding QuillJS to the Posts create page

--- Shared/Models/PostDTO.cs
+++ Shared/Models/PostDTO.cs
@@ -19,7 +19,6 @@ namespace Shared.Models
         [MaxLength(512)]
         public string Excerpt { get; set; }
 
-        [Required]
         [MaxLength(65536)]
         public string Content { get; set; }
 
