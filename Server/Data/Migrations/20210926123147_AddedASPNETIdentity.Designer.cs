1b14ef7f41990fc17c212dfc3c21c8ab9bd1699c
Cullington <70056723+RubenHeeren@users.noreply.github.com>
2021-09-26 14:34:51 +0200

    Updating the database with ASP.NET Identity

--- Server/Data/Migrations/20210926123147_AddedASPNETIdentity.Designer.cs
+++ Server/Data/Migrations/20210926123147_AddedASPNETIdentity.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Server.Data;
 
 namespace Server.Data.Migrations
 {
     [DbContext(typeof(AppDBContext))]
-    partial class AppDBContextModelSnapshot : ModelSnapshot
+    [Migration("20210926123147_AddedASPNETIdentity")]
+    partial class AddedASPNETIdentity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
